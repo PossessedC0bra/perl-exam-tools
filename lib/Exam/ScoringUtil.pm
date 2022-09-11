98e743bb63806dd5e2c105f9f3efc4d596c23f6e
Yannick Koller <koller.yannick@gmail.com>
2022-09-11 20:40:46 +0200

    :sparkles: fuzzy match inexact q/a texts

--- lib/Exam/ScoringUtil.pm
+++ lib/Exam/ScoringUtil.pm
@@ -6,13 +6,17 @@ use warnings;
 use experimental 'signatures';
 
 use Exam::Grammar 'load_exam';
+use Text::Stopwords 'get_stopwords';
 
 use Exporter 'import';
-our @EXPORT = qw(build_question_answer_map);
+our @EXPORT = qw(build_question_answer_map resolve_question_answer_key);
+
+use Text::Levenshtein::Damerau::XS 'xs_edistance';
 
 #####################################################################
 
 my $WHITESPACE_REGEX = qr { \s+ }x;
+my $STOPWORD_REGEX   = qr {\b (?: @{[ join('|', get_stopwords()) ]} ) \b}x;
 
 #####################################################################
 
@@ -27,14 +31,14 @@ sub build_question_answer_map ($file_path) {
 
         foreach my $answer_ref ( @{ $question_ref->{ANSWERS} } ) {
             if ( $answer_ref->{CHECKBOX} =~ m {\[ [Xx] \]}x ) {
-                push( @checked_answers, sanitize_text( $answer_ref->{TEXT} ) );
+                push( @checked_answers, normalize_whitespace( $answer_ref->{TEXT} ) );
             }
             else {
-                push( @unchecked_answers, sanitize_text( $answer_ref->{TEXT} ) );
+                push( @unchecked_answers, normalize_whitespace( $answer_ref->{TEXT} ) );
             }
         }
 
-        $question_answer_map{ sanitize_text( $question_ref->{TEXT} ) } = {
+        $question_answer_map{ normalize_whitespace( $question_ref->{TEXT} ) } = {
             CHECKED   => [@checked_answers],
             UNCHECKED => [@unchecked_answers]
         };
@@ -43,19 +47,67 @@ sub build_question_answer_map ($file_path) {
     return \%question_answer_map;
 }
 
-sub sanitize_text ($text) {
-    my $sanitized_text = lc($text);
+sub resolve_question_answer_key ( $key, %question_answer_map ) {
 
-    # replace sequences of whitespaces with a single space character
-    $sanitized_text =~ s { $WHITESPACE_REGEX } { }gx;
+    # if the master answer is found exactly in the exam file return it
+    if ( exists $question_answer_map{$key} ) {
+        return $key;
+    }
+
+    # otherwise attempt to fuzzy match the master answer against all exam question -> answers
+    return fuzzy_search( $key, keys %question_answer_map );
+}
+
+sub fuzzy_search ( $source_text, @target_texts ) {
+
+    # normalize the master text before comparison
+    my $normalized_source_text = normalize($source_text);
+
+    # accept an edit distance of at most 10% of the normalized master texts total length
+    my $edit_distance_threshold = 0.1 * length($normalized_source_text);
+
+    # try to fuzzy match the master text against all the given exam texts
+    foreach my $target_text (@target_texts) {
+
+        # specifying a threshold means xs_edistance will short circuit and return -1 if the threshold is exceeded
+        if ( xs_edistance( $normalized_source_text, normalize($target_text), $edit_distance_threshold ) != -1 ) {
+            return $target_text;
+        }
+    }
+
+    # no exam question could be fuzzy matched to the master question -> return undef to indicate missing exam question
+    return undef;
+}
+
+#####################################################################
+# TEXT NORMALIZATION
+#####################################################################
+
+sub normalize ($text) {
+
+    # convert whole text to lowercase
+    my $text = lc($text);
+
+    # remove common stopwords
+    $text =~ s { $STOPWORD_REGEX } {}gx;
+
+    return normalize_whitespace($text);
+}
+
+sub normalize_whitespace ($text) {
+
+    # replace sequences of whitespaces with a single space (' ') character
+    $text =~ s { $WHITESPACE_REGEX } { }gx;
 
     # remove whitespace at start
-    $sanitized_text =~ s { ^ $WHITESPACE_REGEX } {}gx;
+    $text =~ s { ^ $WHITESPACE_REGEX } {}gx;
 
     # remove whitespace at end
-    $sanitized_text =~ s { $WHITESPACE_REGEX $ } {}gx;
+    $text =~ s { $WHITESPACE_REGEX $ } {}gx;
 
-    return $sanitized_text;
+    return $text;
 }
+
+#####################################################################
 
 1;    # Magic boolean TRUE value required at end of a module
--- score_exams.pl
+++ score_exams.pl
@@ -5,7 +5,31 @@ use strict;
 use warnings;
 use experimental 'signatures';
 
-use Exam::ScoringUtil 'build_question_answer_map';
+use Exam::ScoringUtil qw(build_question_answer_map fuzzy_search);
+
+#####################################################################
+
+sub resolve_exam_question ( $master_question, %exam_question_answer_map ) {
+
+    # if the master question is found exactly in the exam file return it
+    if ( exists $exam_question_answer_map{$master_question} ) {
+        return $master_question;
+    }
+
+    # otherwise attempt to fuzzy match the master question against all exam questions
+    return fuzzy_search( $master_question, keys %exam_question_answer_map );
+}
+
+sub resolve_exam_answer ( $master_answer, %exam_answer_map ) {
+
+    # if the master answer is found exactly in the exam file return it
+    if ( exists $exam_answer_map{$master_answer} ) {
+        return $master_answer;
+    }
+
+    # otherwise attempt to fuzzy match the master answer against all exam question -> answers
+    return fuzzy_search( $master_answer, keys %exam_answer_map );
+}
 
 #####################################################################
 
@@ -24,59 +48,55 @@ my @results;
 # score each exam (arguments beyond the first) individually
 foreach my $exam_file_path ( @ARGV[ 1 .. $#ARGV ] ) {
     my %result = (
-        FILE              => $exam_file_path,
-        TOTAL_ANSWERS     => 0,
-        CORRECT_ANSWERS   => 0,
-        MISSING_QUESTIONS => [],
-        MISSING_ANSWERS   => {}
+        FILENAME                 => $exam_file_path,    # string
+        TOTAL_ANSWERS            => 0,                  # int
+        CORRECT_ANSWERS          => 0,                  # int
+        MISSING_MASTER_QUESTIONS => {},                 # master_question => exam_question
+        MISSING_MASTER_ANSWERS   => {},                 # master_question => {master_answer => exam_answer}
     );
 
     # build question and answer map for this exam
     my %exam_question_answer_map = %{ build_question_answer_map($exam_file_path) };
 
     # iterate over all questions in the master file
-    foreach my $question ( keys %master_question_answer_map ) {
+    foreach my $master_question ( keys %master_question_answer_map ) {
 
-        # question not found -> remember as missing and move to the next one
-        if ( !exists $exam_question_answer_map{$question} ) {
-            push( @{ $result{MISSING_QUESTIONS} }, $question );
-            next;
+        my $exam_question = resolve_exam_question( $master_question, %exam_question_answer_map );
+
+        # master question was not exactly found in exam file -> remember mapping
+        if ( !$exam_question || $master_question ne $exam_question ) {
+            $result{MISSING_MASTER_QUESTIONS}->{$master_question} = $exam_question;
+
+            # move to the next master question if exam question was not resolved at all
+            next if !$exam_question;
         }
 
-        my @master_checked_answers = @{ $master_question_answer_map{$question}{CHECKED} };
-        my @exam_checked_answers   = @{ $exam_question_answer_map{$question}{CHECKED} };
+        # remember checked answers for later lookup
+        my @checked_master_answers = @{ $master_question_answer_map{$master_question}{CHECKED} };
+        my @checked_exam_answers   = @{ $exam_question_answer_map{$exam_question}{CHECKED} };
 
-        # search for missing answers in exam file
-        my @missing_answers;
-
-        # collect all answers from master file
-        my @master_answers = ( @master_checked_answers, @{ $master_question_answer_map{$question}{UNCHECKED} } );
-
-        # collect all answers from exam file
+        # collect all answers for comparing them against each other
+        my @master_answers = ( @checked_master_answers, @{ $master_question_answer_map{$master_question}{UNCHECKED} } );
         my @exam_answers =
-          ( @exam_checked_answers, @{ $exam_question_answer_map{$question}{UNCHECKED} } );
+          ( @checked_exam_answers, @{ $exam_question_answer_map{$exam_question}{UNCHECKED} // [] } );
 
         # build map of answers in exam file for quick lookups
         my %exam_answers_map = map( ( $_ => 1 ), @exam_answers );
 
         # check master answers against the answers in the exam file
         foreach my $master_answer (@master_answers) {
-            if ( !exists $exam_answers_map{$master_answer} ) {
-                push( @missing_answers, $master_answer );
+            my $exam_answer = resolve_exam_answer( $master_answer, %exam_answers_map );
+            if ( !$exam_answer || $master_answer ne $exam_answer ) {
+                $result{MISSING_MASTER_ANSWERS}->{$master_question}{$master_answer} = $exam_answer;
             }
         }
 
-        # only create a missing answers entry if there are some
-        if (@missing_answers) {
-            $result{MISSING_ANSWERS}{$question} = [@missing_answers];
-        }
-
-        # no answer was provided -> ignore this question
-        if ( !@exam_checked_answers ) {
+        # no answer was provided in the exam -> ignore this question
+        if ( !@checked_exam_answers ) {
             next;
         }
 
-        if ( @exam_checked_answers == 1 && $master_checked_answers[0] eq $exam_checked_answers[0] ) {
+        if ( @checked_exam_answers == 1 && $checked_master_answers[0] eq $checked_exam_answers[0] ) {
             $result{CORRECT_ANSWERS}++;
         }
 
@@ -103,7 +123,7 @@ foreach my $result_ref (@results) {
     #   2 characters for total number of questions
     printf(
         "%-70.70s %02s / %02s\n",
-        $result_ref->{FILE},
+        $result_ref->{FILENAME},
         $result_ref->{CORRECT_ANSWERS},
         $result_ref->{TOTAL_ANSWERS}
     );
@@ -116,32 +136,33 @@ print("\n");
 foreach my $result_ref (@results) {
 
     # print nothing for complete files
-    if ( !@{ $result_ref->{MISSING_QUESTIONS} } && !keys %{ $result_ref->{MISSING_ANSWERS} } ) {
+    if ( !keys %{ $result_ref->{MISSING_MASTER_QUESTIONS} } && !keys %{ $result_ref->{MISSING_MASTER_ANSWERS} } ) {
         next;
     }
 
-    say("$result_ref->{FILE}:");
+    say("$result_ref->{FILENAME}:");
 
     # firstly print all missing questions
-    foreach my $missing_question ( @{ $result_ref->{MISSING_QUESTIONS} } ) {
-        say("\tMissing question: $missing_question");
+    foreach my $missing_question ( keys %{ $result_ref->{MISSING_MASTER_QUESTIONS} } ) {
+        say("\t- Missing question: '$missing_question'");
+        if ( $result_ref->{MISSING_MASTER_QUESTIONS}{$missing_question} ) {
+            say("\t  Used this instead: '$result_ref->{MISSING_MASTER_QUESTIONS}{$missing_question}'");
+        }
     }
 
     # secondly print missing answers
-    foreach my $question ( keys %{ $result_ref->{MISSING_ANSWERS} } ) {
+    foreach my $question ( keys %{ $result_ref->{MISSING_MASTER_ANSWERS} } ) {
 
-        # deference for better readability
-        my @missing_answers = @{ $result_ref->{MISSING_ANSWERS}{$question} };
-
-        # only print message if there are missing answers
-        if ( !@missing_answers ) {
-            next;
-        }
+        # dereference for better readability
+        my %missing_answers_map = %{ $result_ref->{MISSING_MASTER_ANSWERS}{$question} };
 
         # print question for easier "debugging"
-        say("\tMissing answers for question: $question");
-        for my $missing_answer (@missing_answers) {
-            say("\t\t- $missing_answer");
+        say("\t- Missing answers for: '$question'");
+        for my $missing_answer ( keys %missing_answers_map ) {
+            say("\t\t- '$missing_answer'");
+            if ( $missing_answers_map{$missing_answer} ) {
+                say("\t\t   Used this instead: '$missing_answers_map{$missing_answer}'");
+            }
         }
     }
 
